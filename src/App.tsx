4f558609d8ef1328fa428ca5fdaa4c2d3ec7afd9
ABabayaga <alef_rdl@hotmail.com>
2024-11-11 21:28:50 -0400

    updatecomerronofront

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,41 @@
-import {mint} from './Web3Service';
+import { mint } from './Web3Service';
 
 function App() {
 
-  function onBtnClick(){
+  function onBtnClick() {
     mint()
-    .then(() => console.log())
-    .catch(err => alert(err.message));
+      .then((tx) => alert(tx))
+      .catch(err => alert(err.message));
   }
 
 
   return (
     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header className="mb-auto">
-    <div>
-      <h3 className="float-md-start mb-0">ProtoCoin faucet</h3>
-      <nav className="nav nav-masthead justify-content-center float-md-end">
-        <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a className="nav-link fw-bold py-1 px-0" href="#">About</a>
-      </nav>
+      <header className="mb-auto">
+        <div>
+          <h3 className="float-md-start mb-0">ProtoCoin faucet</h3>
+          <nav className="nav nav-masthead justify-content-center float-md-end">
+            <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
+            <a className="nav-link fw-bold py-1 px-0" href="#">About</a>
+          </nav>
+        </div>
+      </header>
+
+      <main className="px-3">
+        <h1> Get your ProtoCoins</h1>
+        <p className="lead">Onde a day, earn 1.000 coins for free just connecting your MetaMask below.</p>
+        <p className="lead">
+          <a href="#" onClick={onBtnClick} className="btn btn-lg btn-light fw-bold border-white bg-white">
+            <img src="/assets/metamask.svg" alt="MetaMask logo" width="48" />
+            Connect MetaMask
+          </a>
+        </p>
+      </main>
+
+      <footer className="mt-auto text-white-50">
+        <p> Built by <a href="https://aleflima.com/mdo" className="text-white">AlefLima</a>.</p>
+      </footer>
     </div>
-  </header>
-
-  <main className="px-3">
-    <h1> Get your ProtoCoins</h1>
-    <p className="lead">Onde a day, earn 1.000 coins for free just connecting your MetaMask below.</p>
-    <p className="lead">
-      <a href="#" onClick={onBtnClick} className="btn btn-lg btn-light fw-bold border-white bg-white">
-        <img src="/assets/metamask.svg" alt="MetaMask logo" width={48} />
-        Connect MetaMask
-        </a>
-    </p>
-  </main>
-
-  <footer className="mt-auto text-white-50">
-    <p> Built by <a href="https://aleflima.com/mdo" className="text-white">AlefLima</a>.</p>
-  </footer>
-</div>
   );
 }
 
--- src/Web3Service.ts
+++ src/Web3Service.ts
@@ -1,11 +1,19 @@
 import Web3 from 'web3';
+import ABI from './abi.json';
+
+const CONTRACT_ADDRESS = `${process.env.REACT_APP_CONTRACT_ADDRESS}`;
 
 export async function mint() {
-    if (!window.ethereum) throw new Error(`No MetaMask found`);
+    if (!window.ethereum) throw new Error(`No MetaMask found!`);
 
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
-    if(!accounts || !accounts.length) throw new Error (`No account allowed!`); 
-    
-    alert(accounts[0]);
+    if (!accounts || !accounts.length) throw new Error(`No account allowed!`);
+
+   
+
+    const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from: accounts[0] });
+    const tx = await contract.methods.mint().send();
+    console.log(tx.transactionHash);
+    return tx.transactionHash;
 }
